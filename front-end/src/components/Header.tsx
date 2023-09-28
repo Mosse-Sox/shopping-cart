021dd08df2a3e5d9fce68e21cc0e83d2615de5af
Mosse-Sox <autumngreenie990@gmail.com>
2023-09-28 17:43:35 -0600

    adds some glow effects when button is hovered in header

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = ({ viewCart, setViewCart }: PropsType) => {
   const content = (
     <header className="header">
       <div className="header__title-bar">
-        <h1>Bean Supply Co.</h1>
+        <h1 className="title">Bean Supply Co.</h1>
       </div>
       <div className="header__price-box">
         <p>Total Items: </p>
--- front-end/src/components/Nav.tsx
+++ front-end/src/components/Nav.tsx
@@ -7,7 +7,7 @@ const Nav = ({ viewCart, setViewCart }: PropsType) => {
   const button = viewCart ? (
     <button onClick={() => setViewCart(false)}>View Products</button>
   ) : (
-    <button onClick={() => setViewCart(true)}>View Cart</button>
+    <button className="button__set-view" onClick={() => setViewCart(true)}>View Cart</button>
   );
 
   const content = <nav className="nav">{button}</nav>;
